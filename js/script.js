111efc157c47b0737bf38270b3c9b2a2ab7b7156
Gosia <malgorzata.krysztofik@gmail.com>
2019-11-29 22:24:13 +0100

    Finish task 6.3 Module

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
-    // console.log(event);
     event.preventDefault();
     const clickedElement = this;
-    // console.log('this',clickedElement);
   
     /* [DONE] remove class 'active' from all article links  */
-
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
@@ -16,28 +12,30 @@ const titleClickHandler = function(event){
     /* [DONE] add class 'active' to the clicked link */
     clickedElement.addEventListener('click',function(){
         clickedElement.classList.add('active'); 
+        /*
+        ! czasem dopiero po drugim kliknieciu pokazuje mi sie klasa active ???
+        */
     });
 
     /* [DONE] remove class 'active' from all articles */
-
     const activeArticles = document.querySelectorAll('.posts article.active');
-    
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
-
+    /* [DONE] get 'href' attribute from the clicked link */
     const getHrefLink = clickedElement.getAttribute('href');
     console.log('getHrefLink zwrócił: ', getHrefLink);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const findArticle = document.querySelector(getHrefLink);
     console.log('znalazlam artykul :3', findArticle);
 
-    /* add class 'active' to the correct article */ 
+    /* [DONE] add class 'active' to the correct article */ 
     findArticle.classList.add('active');
+    /* 
+    ! Czy taka metoda jest poprawna? 
+    */
   }
   
   const links = document.querySelectorAll('.titles a');
@@ -45,5 +43,4 @@ const titleClickHandler = function(event){
   for(let link of links){
     const clickedElement = this;
     link.addEventListener('click', titleClickHandler);
-    // console.log('clickedElement (with plus): ' + clickedElement);
   }  
